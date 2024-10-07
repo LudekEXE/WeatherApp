959ef9ca224ee6f00c4ec13219ae2f1cb55b2065
unknown <szymon>
2024-10-07 09:36:31 +0200

    git-ignore

--- WeatherApp/obj/Debug/net8.0/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/obj/Debug/net8.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ca2856c16a91d7e677c9bcfc14845624cffd85")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
